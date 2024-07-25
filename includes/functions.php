3076507f9d2c2b7c20a3a1264e23efdbfd8a00a8
Alvaro Franz <mail@alvarofranz.com>
2024-07-25 13:19:48 +0200

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,34 +1,16 @@
 <?php
 
 // Function to send an email using SendGrid
-function send_email($email_address, $email_subject, $email_content, $attachments = []): bool
+function send_email($email_address, $email_subject, $email_content): bool
 {
     $email = new \SendGrid\Mail\Mail();
     $email->setFrom("info@dailyphrase.email", "Daily Phrase");
     $email->setSubject($email_subject);
     $email->addTo($email_address);
     $email->addContent("text/html", $email_content);
-    // Add attachments
-    foreach ($attachments as $attachment) {
-        $email->addAttachment(
-            $attachment['base64'],
-            $attachment['type'],
-            $attachment['filename'],
-            $attachment['disposition']
-        );
-    }
     $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
     try {
         $sendgrid->send($email);
-
-        // Append attachment filenames to the email content if any
-        if (count($attachments) > 0) {
-            $email_content .= '<br><strong>Attachments:</strong>';
-            foreach ($attachments as $attachment) {
-                $email_content .= '<br>' . $attachment['filename'];
-            }
-        }
-
         return true;
     } catch (Exception $e) {
         return false;
@@ -36,7 +18,7 @@ function send_email($email_address, $email_subject, $email_content, $attachments
 }
 
 // Function to generate a token
-function generateToken($id, $email)
+function generateToken($id, $email): string
 {
     return hash('sha256', $id . $email);
 }
