bf5f628ca2d2351f186e681f7c43ec27b88ad149
Alvaro Franz <mail@alvarofranz.com>
2024-08-09 10:52:54 +0200

    Update email again

--- public/index.php
+++ public/index.php
@@ -82,7 +82,7 @@ if ($view === 'default_view' && $_SERVER['REQUEST_METHOD'] === 'POST' && isset($
 
         $welcome_email = '<h1>Thanks for joining Daily Phrase!</h1>
         <p>Please confirm your email clicking the link below, in order to start receiving your daily phrases:</p>
-        <p><a href="' . $verification_link . '" style="background-color:#1b74e4;text-decoration:none;display:block;border-radius:5px;font-size: 16px;"><span style="font-family:Helvetica,sans-serif;white-space:nowrap;font-weight:bold;vertical-align:middle;color:white;line-height:16px;">Confirm&nbsp;now</span></a></p><p>You will enjoy the daily phrases :)</p><hr>
+        <p><a href="' . $verification_link . '" style="background-color:#1b74e4;text-decoration:none;display:inline-block;padding:10px 16px;border-radius:5px;font-size: 16px;font-family:Helvetica,sans-serif;font-weight:bold;color:#FFFFFF;line-height:16px;">Confirm&nbsp;now</a></p><p>You will enjoy the daily phrases :)</p><hr>
         <p style="margin-top:30px;font-size:11px;color:#555;">30 N Gould St Ste N, Sheridan, WY 82801</p>';
 
         // Send verification email (Use your own mail function or mail library)
